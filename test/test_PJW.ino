6b6fc0d9d379cb85450f58f887493c2aabc8278d
qsc0415 <71138650+qsc0415@users.noreply.github.com>
2022-02-23 20:57:17 +0900

    0223 - 1층 lift_up해서 위에 올려놓고 빼기

--- test/test_PJW.ino
+++ test/test_PJW.ino
@@ -7,6 +7,8 @@ EXPANSION exc2;
 void wheel(int x, int y, int z);
 void LineTracing();
 void LineTraceing();
+void lift_up(int s);
+void lift_down(int s);
 void start();
 void turn();
 int n = 0;
@@ -19,13 +21,19 @@ void setup()
 
 void loop()
 {
-  start();
-  LineTracing(50);
-  lift_up();
-  wheel(0, 80, 0);
-  delay(600);
+  //start();
+  //LineTracing(40);
+  lift_up(4700);
+  LineTracing(40);
+  //lift_up(4400); //내려놓는 위치
+  lift_down(350);
+  
+  wheel(0,50,0);
+  delay(1000);
+  //wheel(0, 80, 0);
+  //delay(600);
   //turn();
-  LineTracing(50);
+  //LineTracing(50);
   prizm.PrizmEnd();
 }
 
@@ -61,41 +69,42 @@ void turn()
   }
 }
 
-int LineTraceing()
+void LineTracing(int speed)
 {
-  int a1 = analogRead(A1);
-  int a2 = analogRead(A2);
-  int D2 = prizm.readLineSensor(2);
-  int D3 = prizm.readLineSensor(3);
-  int D4 = prizm.readLineSensor(4);
-  print(a1, a2, D2, D3, D4);
-
-  if (D3 == HIGH && D4 == LOW)
+  while (1)
   {
-    // D3 감지되면 왼쪽 앞
-    wheel(0, -40, -5);
-    return 0;
-  }
+    Serial.println("Line Tracing...");
+    int a1 = analogRead(A1);
+    int a2 = analogRead(A2);
+    int D2 = prizm.readLineSensor(2);
+    int D3 = prizm.readLineSensor(3);
+    int D4 = prizm.readLineSensor(4);
 
-  if (D3 == LOW && D4 == HIGH)
-  {
-    // D4 감지되면 오른쪽 앞
-    wheel(0, -40, 5);
-    return 0;
-  }
 
-  if (D3 == LOW && D4 == LOW)
-  {
-    // D3, D4 감지 안되면 직진
-    wheel(0, -40, 0);
-    return 0;
-  }
+    if (D3 == HIGH && D4 == LOW)
+    {
+      // D3 감지되면 왼쪽 앞
+      wheel(0, -speed, -speed * 0.15);
+    }
 
-  if (D3 == HIGH && D4 == HIGH)
-  {
-    // D3, D4 감지되면 정지
-    // wheel(0, 0, 0);
-    return 1;
+    if (D3 == LOW && D4 == HIGH)
+    {
+      // D4 감지되면 오른쪽 앞
+      wheel(0, -speed, speed * 0.15);
+    }
+
+    if (D3 == LOW && D4 == LOW)
+    {
+      // D3, D4 감지 안되면 직진
+      wheel(0, -speed, 0);
+    }
+
+    if (D3 == HIGH && D4 == HIGH)
+    {
+      // D3, D4 감지되면 정지
+      wheel(0, 0, 0);
+      break;
+    }
   }
 }
 
